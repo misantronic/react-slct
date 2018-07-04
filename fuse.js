1070614f10bf81b2f84f1affb8be00b87cd2365d
David Schkalee <david.schkalee@magicline.de>
2018-07-04 23:47:43 +0200

    add minify

--- fuse.js
+++ fuse.js
@@ -1,13 +1,27 @@
 const {
-    FuseBox
+    FuseBox,
+    QuantumPlugin
 } = require("fuse-box");
 
 const fuse = FuseBox.init({
-    homeDir: "src",
+    homeDir: 'src',
+    target: 'browser@es2015',
+    sourceMaps: true,
+    output: "dist/$name.js"
+});
+
+const fuse2 = FuseBox.init({
+    homeDir: 'src',
     target: 'browser@es2015',
     sourceMaps: true,
     output: "dist/$name.js",
+    plugins: [QuantumPlugin({
+        uglify: true,
+        treeshake: true
+    })]
 })
 
 fuse.bundle("react-slct").instructions(" > [index.tsx]")
+fuse2.bundle("react-slct.min").instructions(" > [index.tsx]")
 fuse.run();
+fuse2.run();
