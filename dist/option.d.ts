2964ed8652ba2e261182c83f58a7b13fe0dec240
David Schkalee <david.schkalee@magicline.de>
2018-10-10 16:20:57 +0200

    compile

--- dist/option.d.ts
+++ dist/option.d.ts
@@ -6,7 +6,7 @@ interface OptionItemProps {
     height?: OptionComponentProps['height'];
 }
 export declare class OptionComponent extends React.PureComponent<OptionComponentProps> {
-    static OptionItem: import("../../../../../Users/dschkalee/src/react-slct/node_modules/styled-components").StyledComponentClass<React.ClassAttributes<HTMLDivElement> & React.HTMLAttributes<HTMLDivElement> & OptionItemProps, any, React.ClassAttributes<HTMLDivElement> & React.HTMLAttributes<HTMLDivElement> & OptionItemProps>;
+    static OptionItem: import("styled-components").StyledComponentClass<React.ClassAttributes<HTMLDivElement> & React.HTMLAttributes<HTMLDivElement> & OptionItemProps, any, React.ClassAttributes<HTMLDivElement> & React.HTMLAttributes<HTMLDivElement> & OptionItemProps>;
     render(): React.ReactNode;
     private onClick;
 }
--- dist/value-component-single.d.ts
+++ dist/value-component-single.d.ts
@@ -1,3 +1,2 @@
-/// <reference types="react" />
 import { ValueComponentSingleProps } from './typings';
 export declare const ValueComponentSingle: (props: ValueComponentSingleProps<any>) => JSX.Element;
