70918c3358f46ede584f59e0342b8e08d4d410c4
david.schkalee <david.schkalee@sportalliance.com>
2023-09-14 01:26:38 +0200

    chore: typings

--- src/menu.tsx
+++ src/menu.tsx
@@ -135,7 +135,7 @@ export function Menu(props: MenuComponentProps) {
                 itemCount={itemCount}
                 itemData={itemData}
             >
-                {MenuRow as any}
+                {MenuRow}
             </FixedSizeList>
         );
     }
